97f5e11d13e984fa2c6c2ea290cbe1a45f2c409a
sigee <minshigee@gmail.com>
2023-03-28 19:11:56 +0900

    nginx configuration

--- client/start-web.sh
+++ client/start-web.sh
@@ -12,41 +12,8 @@ install_nginx()
 
 write_nginx_configuration()
 {
-    sudo mkdir -p /data/logs/nginx/${HOSTNAME}
-    sudo chown -R ubuntu:ubuntu /data/logs/nginx/${HOSTNAME}
-
     cat << EOF | sudo tee /etc/nginx/sites-available/default
-log_format main escape=json
-  '{'
-    '"time_local":"\$time_local",'
-    '"remote_addr":"\$remote_addr",'
-    '"remote_user":"\$remote_user",'
-    '"request":"\$request",'
-    '"status": "\$status",'
-    '"body_bytes_sent":"\$body_bytes_sent",'
-    '"request_time":"\$request_time",'
-    '"http_referrer":"\$http_referer",'
-    '"http_user_agent":"\$http_user_agent"'
-  '}';
-server {
-    listen 80 default_server;
-    access_log /data/logs/nginx/${HOSTNAME}/access.log main;
-    error_log /data/logs/nginx/${HOSTNAME}/error.log;
-    location / {
-        root   /usr/share/nginx/html;
-        index  index.html;
-        try_files \$uri \$uri/ /index.html;
-    }
-    location /api {
-        proxy_pass ${APP_ENDPOINT};
-        proxy_http_version 1.1;
-        proxy_redirect off;
-    }
-    error_page   500 502 503 504  /50x.html;
-    location = /50x.html {
-        root   /usr/share/nginx/html;
-    }
-}
+    $(cat ./confs/nginx.conf)
 EOF
     sudo service nginx reload
 }
